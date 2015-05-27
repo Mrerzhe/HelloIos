7ed6032eaa108bcd526eb3cfc62e5cb881f9af95
三俗小青年 <liuyu@yudeMacBook-Pro.local>
2015-05-27 19:31:20 +0800

    Initial Commit

---
+++ HelloIos/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HelloIos
+//
+//  Created by 三俗小青年 on 15/4/30.
+//  Copyright (c) 2015年 三俗小青年. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+            println(">>>>")
+     
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ 小青年/小青年/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  小青年
+//
+//  Created by 三俗小青年 on 15/5/27.
+//  Copyright (c) 2015年 三俗小青年. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
